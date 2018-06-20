70f9ce3315e5e1d8f702260a0a035b1b9d722dbe
Amy Tu <siamesebb@gmail.com>
2018-06-20 02:35:22 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,8 +2,17 @@ class Artist
   
   attr_accessor :name 
   
+  @@all = []
+  
   def initialize(name)
     @name = name 
     @songs = []
+    @@all << self
   end
+  
+  def songs 
+    @songs
+  end
+  
+  
 end
