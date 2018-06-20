1d3f6791c09784d1c23fc44f21b053b9a56eb156
Amy Tu <siamesebb@gmail.com>
2018-06-20 03:50:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,8 +9,6 @@ class Song
   def self.new_by_filename(file_name)
     split_name =file_name.split(" - ")
     song = self.new(split_name[1])
-    new_song = self.new(song)
-    new_song.artist = artist
-    new_song 
+    artist = Artist. 
   end
 end
